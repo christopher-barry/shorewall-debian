990440c10dcf0ce80825dd7329b35c3f29c215fd
Roberto C. Sanchez <roberto@connexer.com>
2013-11-28 20:11:16 -0500

    Imported Upstream version 4.5.21.4

--- Perl/Shorewall/Config.pm
+++ Perl/Shorewall/Config.pm
@@ -687,7 +687,7 @@ sub initialize( $;$$) {
 		    TC_SCRIPT               => '',
 		    EXPORT                  => 0,
 		    KLUDGEFREE              => '',
-		    VERSION                 => "4.5.21.3",
+		    VERSION                 => "4.5.21.4",
 		    CAPVERSION              => 40515 ,
 		  );
     #
